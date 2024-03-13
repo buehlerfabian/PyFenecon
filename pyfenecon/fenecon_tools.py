c3c8481e1d50180e9f6d4c28d75a4ad50c4790ec
Fabian Bühler <buehler.fabian@gmx.de>
2024-03-13 08:56:02 +0100

    import config file

--- pyfenecon/fenecon_tools.py
+++ pyfenecon/fenecon_tools.py
@@ -1,2 +1,20 @@
+import toml
+import os
+
+# read config file
+try:
+    with open('pyfeneconrc', 'r') as f:
+        config = toml.load(f)
+except FileNotFoundError:
+    home = os.path.expanduser('~')
+    try:
+        with open(f'{home}/.config/pyfeneconrc', 'r') as f:
+            config = toml.load(f)
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            'No pyfeneconrc file found in working directory'
+            ' or in ~/.config directory.')
+
+
 def get_status():
     pass
