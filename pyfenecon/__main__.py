e05a623b3444edf24beea7a7af01e61b42294143
Fabian Bühler <buehler.fabian@gmx.de>
2024-03-13 08:24:25 +0100

    create skeleton files

---
+++ pyfenecon/__main__.py
@@ -0,0 +1,16 @@
+import sys
+import argparse
+
+
+def main():
+    # set up parser
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args() # noqa F841
+
+    # process arguments
+
+    # print current status
+
+
+if __name__ == "__main__":
+    sys.exit(main())
