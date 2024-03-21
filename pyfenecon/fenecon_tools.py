3c58e3a236ef24977e78e321539d19b65ffb3915
Fabian Bühler <buehler.fabian@gmx.de>
2024-03-21 21:49:42 +0100

    Add get_status function to retrieve all fields

--- pyfenecon/fenecon_tools.py
+++ pyfenecon/fenecon_tools.py
@@ -22,6 +22,15 @@ REST_USER = "x"
 REST_PASSWORD = "user"
 
 
+def get_status():
+    session = requests.Session()
+    session.auth = (REST_USER, REST_PASSWORD)
+    response = session.get(f"{REST_URL}/.*/.*")
+    response.raise_for_status()
+    session.close()
+    return json.loads(response.text)
+
+
 def get_state_of_charge():
     session = requests.Session()
     session.auth = (REST_USER, REST_PASSWORD)
